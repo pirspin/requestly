fbdb6f471f2a6f5da5d2d7d13f10d1485ce59e77
nafees87n <nafees87n@gmail.com>
2025-08-20 14:29:57 +0530

    [ENGG-4128] integrated refactored fetcher with existing code

--- app/src/hooks/DbListenerInit/availableTeamsListener.js
+++ app/src/hooks/DbListenerInit/availableTeamsListener.js
@@ -39,35 +39,6 @@ const availableTeamsListener = (dispatch, uid, activeWorkspaceId, appMode, isLoc
     return onSnapshot(
       q,
       async (querySnapshot) => {
-        // let localRecords = [];
-        // if (isLocalSyncEnabled) {
-        // const allLocalWorkspacesResult = await getAllWorkspaces();
-        // const allLocalWorkspaces =
-        //   allLocalWorkspacesResult.type === "success" ? allLocalWorkspacesResult.content : [];
-
-        // submitAttrUtil(APP_CONSTANTS.GA_EVENTS.ATTR.NUM_LOCAL_WORKSPACES, allLocalWorkspaces.length);
-
-        // for (const partialWorkspace of allLocalWorkspaces) {
-        //   const localWorkspace = {
-        //     id: partialWorkspace.id,
-        //     name: partialWorkspace.name,
-        //     owner: uid,
-        //     accessCount: 1,
-        //     adminCount: 1,
-        //     members: {
-        //       [uid]: {
-        //         role: "admin",
-        //       },
-        //     },
-        //     appsumo: null,
-        //     workspaceType: WorkspaceType.LOCAL,
-        //     rootPath: partialWorkspace.path,
-        //   };
-
-        //   localRecords.push(localWorkspace);
-        // }
-        // }
-
         const records = querySnapshot.docs
           .map((team) => {
             const teamData = team.data();
@@ -107,12 +78,12 @@ const availableTeamsListener = (dispatch, uid, activeWorkspaceId, appMode, isLoc
             return formattedTeamData;
           })
           .filter(Boolean);
-        // records.push(...localRecords);
-        // dispatch(teamsActions.setAvailableTeams(records));
         dispatch(workspaceActions.upsertManyWorkspaces(records));
 
         if (!activeWorkspaceId) return;
 
+        //FIX ME: the following code's intention is unclear
+        //Showing an alert is unnecessary
         const found = records.find((team) => team.id === activeWorkspaceId);
 
         Logger.log("DBG: availableTeamsListener", {
--- app/src/layouts/DashboardLayout/index.jsx
+++ app/src/layouts/DashboardLayout/index.jsx
@@ -26,6 +26,7 @@ import { useCurrentWorkspaceUserRole } from "hooks";
 import { TeamRole } from "types";
 import { Conditional } from "components/common/Conditional";
 import { MenuHeader } from "./MenuHeader/MenuHeader";
+import { useWorkspaceFetcher } from "hooks/workspaceFetcher/useWorkspaceFetcher";
 
 const DashboardLayout = () => {
   const dispatch = useDispatch();
@@ -38,6 +39,7 @@ const DashboardLayout = () => {
   const isReadRole = role === TeamRole.read;
 
   useRootPathRedirector();
+  useWorkspaceFetcher();
   // initializeOneTap();
 
   // if (shouldShowOneTapPrompt()) {
